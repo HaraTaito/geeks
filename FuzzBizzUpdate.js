eefc3a70926369775e1f7138cd9990e2c6ac9322
lanperu <hara.taito@geekss-it.com>
2020-07-20 11:33:53 +0900

    changed console.log

--- FuzzBizzUpdate.js
+++ FuzzBizzUpdate.js
@@ -1,13 +1,16 @@
 //JS ふぁずびず問題（JS）
 
+const myfunc = function fizzbuzz(){
 for (let i = 1; i <= 100; i++) {
 　 if(i % 15 === 0){
-       console.log("FizzBuzz");
+       document.write("FizzBuzz");
    } else if (i % 5 === 0){
-        console.log("Buzz");
+        document.write("Buzz");
    } else if(i % 3 === 0){
-        console.log("Fizz");
+        document.write("Fizz");
    } else {
-      console.log(i);
+      document.write(i);
+
    }
  }
+}
