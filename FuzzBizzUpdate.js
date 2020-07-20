13585fb58a62bb0f808624fba85e62d9435703b1
lanperu <hara.taito@geekss-it.com>
2020-07-20 17:31:20 +0900

    change onclick addEventListener

--- FuzzBizzUpdate.js
+++ FuzzBizzUpdate.js
@@ -2,15 +2,15 @@
 
 const myfunc = document.getElementById("myfunc");
 myfunc.addEventListener("click", function() {
-for (let i = 1; i <= 100; i++) {
-　 if(i % 15 === 0){
-       document.write("FizzBuzz");
-   } else if (i % 5 === 0){
-        document.write("Buzz");
-   } else if(i % 3 === 0){
-        document.write("Fizz");
-   } else {
-      document.write(i);
-   } document.write("</br>");
- }
+  for (let i = 1; i <= 100; i++) {
+  　 if(i % 15 === 0){
+         document.write("FizzBuzz");
+     } else if (i % 5 === 0){
+          document.write("Buzz");
+     } else if(i % 3 === 0){
+          document.write("Fizz");
+     } else {
+        document.write(i);
+     } document.write("</br>");
+   }
 });
