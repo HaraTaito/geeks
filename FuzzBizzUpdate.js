d8c359449ba532e8ba3f307d5394f109291a430e
lanperu <hara.taito@geekss-it.com>
2020-07-21 16:22:32 +0900

    countTask

--- FuzzBizzUpdate.js
+++ FuzzBizzUpdate.js
@@ -1,15 +1,19 @@
 //JS ふぁずびず問題（JS）
-let num = "myfuncNum"
+
 const myfunc = document.getElementById("myfunc");
 myfunc.addEventListener("click", function() {
+let num = document.getElementById("myfuncNum").value;
   for (let i = 1; i <= num; i++) {
-  　 if(i % 15 === 0){
+     if(num >= 1001){
+      document.write("1000以上は表示されません");
+      break;
+    }else if(i % 15 === 0){
          document.write("FizzBuzz");
      } else if (i % 5 === 0){
           document.write("Buzz");
      } else if(i % 3 === 0){
           document.write("Fizz");
-     } else {
+     }else {
         document.write(i);
      } document.write("</br>");
    }
