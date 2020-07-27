6d1033b749ec77ed3aeb354d172e3d32abbb79f4
lanperu <hara.taito@geekss-it.com>
2020-07-27 18:04:38 +0900

    なぜ指定した数字が変化しないんだ

--- FuzzBizzUpdate.js
+++ FuzzBizzUpdate.js
@@ -4,6 +4,8 @@ const myfunc = document.getElementById("myfunc");
 myfunc.addEventListener("click", function() {
 const num = document.getElementById("myfuncNum").value;
 document.getElementById('result').textContent = null; //リストの初期化
+const fizzNum = document.getElementById("Fizznum");
+const buzzNum = document.getElementById("Buzznum");
 for (let i = 1; i <= num; i++) {
     if(num >= 1001){
           break;
@@ -11,11 +13,11 @@ for (let i = 1; i <= num; i++) {
         const li = document.createElement('li'); //liノードを作成
         li.textContent ="FizzBuzz";　//liに文字列を入れる
         document.getElementById('result').appendChild(li);//resultにliを追加。以下繰り返し
-     } else if (i % 5 === 0){
+     } else if (i === buzzNum){
         const li = document.createElement('li');
         li.textContent ="Buzz";
         document.getElementById('result').appendChild(li);
-     } else if(i % 3 === 0){
+     } else if(i === fizzNum){
         const li = document.createElement('li');
         li.textContent ="Fizz";
         document.getElementById('result').appendChild(li);
