90ca79fffb7cce36b7b25486d2b8f7e718ac0fae
lanperu <hara.taito@geekss-it.com>
2020-07-27 16:06:42 +0900

    7/27課題

--- FuzzBizzUpdate.js
+++ FuzzBizzUpdate.js
@@ -3,17 +3,26 @@
 const myfunc = document.getElementById("myfunc");
 myfunc.addEventListener("click", function() {
 const num = document.getElementById("myfuncNum").value;
+document.getElementById('result').textContent = null; //リストの初期化
   for (let i = 1; i <= num; i++) {
      if(num >= 1001){
           break;
     }else if(i % 15 === 0){
-         document.write("FizzBuzz");
+      const li = document.createElement('li'); //liノードを作成
+         li.textContent ="FizzBuzz";　//liに文字列を入れる
+          document.getElementById('result').appendChild(li);//resultにliを追加。以下繰り返し
      } else if (i % 5 === 0){
-          document.write("Buzz");
+       const li = document.createElement('li');
+          li.textContent ="Buzz";
+          document.getElementById('result').appendChild(li);
      } else if(i % 3 === 0){
-          document.write("Fizz");
+       const li = document.createElement('li');
+          li.textContent ="Fizz";
+          document.getElementById('result').appendChild(li);
      }else {
-        document.write(i);
-     } document.write("</br>");
+       const li = document.createElement('li');
+      li.textContent = i;
+      document.getElementById('result').appendChild(li);
+     }
    }
-});
+} );
