b3e4d4a4ac94d2d1bc9da78189762122ad9b752c
lanperu <hara.taito@geekss-it.com>
2020-07-30 16:28:45 +0900

    jQuery書き換え完了

--- jQueryFizzBuzz.js
+++ jQueryFizzBuzz.js
@@ -1,33 +1,44 @@
 $(function(){
   $('#myfunc').click(function(){
-    location.relord();
     const myfuncNum = $('#myfuncNum').val(); //for文最大値　1000以上の時エラーメッセージを出すコーディング
     const fizzNum = $('#Fizznum').val(); //倍数A;
     const buzzNum = $('#Buzznum').val(); //倍数B;
     const wordA = $('#wordA').val(); //Aの変換文字
     const wordB = $('#wordB').val(); //Bの変換文字
-
-    for(let i = 1; i <= myfuncNum; i++) {
-      if(myfuncNum >=1001 ){
-        break;
-      } else if(i % (fizzNum * buzzNum) ===0){
-          const list = $('<li>');
-          list.html('公倍数');
-          $('#result').append(list);
-      } else if(i % fizzNum ===0){
-          const list = $('<li>');
-          list.html(wordA);
-          $('#result').append(list);
-      } else if(i % buzzNum ===0){
-          const list = $('<li>');
-          list.html(wordB);
-          $('#result').append(list);
-      } else {
-        const list = $('<li>');
-        list.html(i);
-        $('#result').append(list);
+//エラーメッセージが一番外のif文。全部通ってfor文が起動
+    if(myfuncNum ===''){
+      $('#error-message0').text('※最大値を入力してください');
+      } else if(fizzNum ===''){
+      $('#error-message1').text('※倍数Aを入力してください');
+      } else if(wordA ===''){
+      $('#error-message2').text('※変化後の文字を入力してください');
+      } else if(buzzNum ===''){
+      $('#error-message3').text('※倍数Bを入力してください');
+      } else if(wordB ===''){
+      $('#error-message4').text('※変更後の文字を入力してください');
+    } else {
+        for(let i = 1; i <= myfuncNum; i++) {
+          if(myfuncNum >=1001 ){
+            break;
+          } else if(i % (fizzNum * buzzNum) ===0){
+              const list = $('<li>');
+              list.html('公倍数');
+              $('#result').append(list);
+          } else if(i % fizzNum ===0){
+              const list = $('<li>');
+              list.html(wordA);
+              $('#result').append(list);
+          } else if(i % buzzNum ===0){
+              const list = $('<li>');
+              list.html(wordB);
+              $('#result').append(list);
+          } else {
+            const list = $('<li>');
+            list.html(i);
+            $('#result').append(list);
+          }
+        }
       }
-    }
   });
 
 });
