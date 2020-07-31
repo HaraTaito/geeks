7b316b4e14b705c19c67c7e09bfdfdf97b892269
lanperu <hara.taito@geekss-it.com>
2020-07-31 16:06:04 +0900

    フォーム追加途中

--- jQueryFizzBuzz.js
+++ jQueryFizzBuzz.js
@@ -2,22 +2,22 @@ $(function(){
   $('#myfunc').click(function(){
     $('#result').empty();
     const myfuncNum = $('#myfuncNum').val(); //for文最大値　1000以上の時エラーメッセージを出すコーディング
-    const fizzNum = $('.new-form').children(); //倍数A;
+    const fizzNum = $('.new-form').find(); //倍数A;
     const buzzNum = $('#Buzznum').val(); //倍数B;
     const wordA = $('#wordA').val(); //Aの変換文字
     const wordB = $('#wordB').val(); //Bの変換文字
 //エラーメッセージが一番外のif文。全部通ってfor文が起動
-    if(myfuncNum ===''){
-      $('#error-message0').text('※最大値を入力してください');
-    } else if(fizzNum ===''){
-      $('#error-message1').text('※倍数Aを入力してください');
-    } else if(wordA ===''){
-      $('#error-message2').text('※変化後の文字を入力してください');
-    } else if(buzzNum ===''){
-      $('#error-message3').text('※倍数Bを入力してください');
-    } else if(wordB ===''){
-      $('#error-message4').text('※変更後の文字を入力してください');
-    } else {
+    // if(myfuncNum ===''){
+    //   $('#error-message0').text('※最大値を入力してください');
+    // } else if(fizzNum ===''){
+    //   $('#error-message1').text('※倍数Aを入力してください');
+    // } else if(wordA ===''){
+    //   $('#error-message2').text('※変化後の文字を入力してください');
+    // } else if(buzzNum ===''){
+    //   $('#error-message3').text('※倍数Bを入力してください');
+    // } else if(wordB ===''){
+    //   $('#error-message4').text('※変更後の文字を入力してください');
+    // } else {
         for(let i = 1; i <= myfuncNum; i++) {
           if(myfuncNum >=1001 ){
             break;
@@ -39,7 +39,7 @@ $(function(){
             $('#result').append(list);
           }
         }
-      }
+      // }
   });
 
   $('.form-delete').click(function(){
@@ -49,7 +49,7 @@ $(function(){
 
   $('.form-create').click(function(){
     //入力フォームを含めた要素、たぶんdivごと追加
-    $('.new-from').append('<span >変化させる値：</span><input type="number" min="1" max="1000">');
+    $('.allform').append('<span >変化させる値：</span><input type="number" min="1" max="1000">');
         $('.new-from').append('<button class="form-delete">削除</button>');
   });
 
