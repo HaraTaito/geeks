9d9c493073c79e4929ecc9b4551bc84f12f3afa6
lanperu <hara.taito@geekss-it.com>
2020-08-17 13:50:16 +0900

    全体修正

--- jQueryFizzBuzz.js
+++ jQueryFizzBuzz.js
@@ -1,40 +1,30 @@
 $(function(){
   $('#myfunc').click(function(){
     $('#result').empty();//ボタンを押すごとにリストを空にする。リストの連続表示を防ぐ
-      const myfuncNum = $('#myfuncNum').val();//for文最大値
-      const nums = $('.new-form').find('.num');
-      const texts = $('.new-form').find('.text');
-      const numArray = [];
-      const textArray = [];
-      let a = 1;
+    const inputArray = [];//初期化とリセット
+    const myfuncNum = $('#myfuncNum').val();//for文最大値
 
-      for(let i = 0; i < nums.length; i++) {
-        numArray.push(nums[i].value);//配列って概念がfindにないからindexに順番に入れているらしい
+    $('.new-form').each(function(index, element){
+      let num = $(element).find('.num').val();
+      let text = $(element).find('.text').val();
+      let rensou = {numKey : num, textKey : text};
+
+      inputArray.push(rensou);//配列に連想配列を1ペアずつ入れいている。キー名は一緒
+      //[0番号　nums:x tezts:y・・・がフォームの数だけある]
+    });
+
+    for(let i = 1; i <= myfuncNum; i++) {
+      let result = "";//初期化
+      for(let j = 0; j < inputArray.length; j++) {
+        if(i % inputArray[j].numKey === 0){
+          result += inputArray[j].textKey;//文字列の代入と連結どっちもできる。
+        }
       }
-
-      for(let i = 0; i < texts.length; i++) {
-        textArray.push(texts[i].value); //上記と同じ
+        if(result ==="") {
+          result = i;
       }
-
-      for(let i = 1; i <= myfuncNum; i++) {
-        if(myfuncNum >= 1001){
-          break;
-        }
-
-        let flag = Boolean(false);
-            const list = $('<li>');
-          for(let j = 0; j < nums.length; j++){
-             if(i % nums[j].value === 0){
-               list.append(textArray[j]);
-               flag = Boolean(true);
-          }
-        }
-          if(flag ===false){
-            list.html(i);
-
-          }
-          $('#result').append(list);
-        }
+            $('#result').append('<li>' + result + '</li>');
+  }
 });
 
   $(document).on("click", '.form-delete', function () {
